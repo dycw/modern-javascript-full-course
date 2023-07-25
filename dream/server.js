d911fb9981ab271e8fe77e15d0cac20846d212ac
Derek Wan <d.wan@icloud.com>
2023-07-25 16:55:29 +0900

    Add `dream` app (#3)

---
+++ dream/server.js
@@ -0,0 +1,27 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.post("/dream", async (req, res) => {
+  const prompt = req.body.prompt;
+  const aiResponse = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  const image = aiResponse.data.data[0].url;
+  res.send({ image });
+});
+app.listen(8080, () => console.log("make art on http://localhost:8080/dream"));
